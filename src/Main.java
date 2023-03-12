6d7bed8f7d1b7f0279d2a2db66c395222927c982
LamTung <kieutunglam1995@gmail.com>
2023-03-12 21:03:37 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(2.0);
+        System.out.println("Radius: " + circle.getRadius());
+        System.out.println("Area: " + circle.getArea());
+    }
+}
+
+class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
